4fa1a1deb72c0c44ee328f1b9011a652a683616c
xRowe <33739292+xRowe@users.noreply.github.com>
2021-10-07 17:10:01 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -5,7 +5,10 @@
 #cp -rf OpenClash/luci-app-openclash package/
 #rm -rf OpenClash
 
+# try to Compile the Driver for i350
 git reset --hard cdb022d3e1cc0a57d16fdfa013382431118a9559
+mkdir package/kernel/i350
+touch package/kernel/i350/Makefile
 
 # Uncomment a feed source
 sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
