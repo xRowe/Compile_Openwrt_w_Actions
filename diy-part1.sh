d4055504e9c09df384948bac81c38273fd220e4f
xRowe <33739292+xRowe@users.noreply.github.com>
2021-10-07 16:31:15 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -5,5 +5,7 @@
 #cp -rf OpenClash/luci-app-openclash package/
 #rm -rf OpenClash
 
+git reset --hard cdb022d3e1cc0a57d16fdfa013382431118a9559
+
 # Uncomment a feed source
 sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
