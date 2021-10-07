cc0946630cb9d9eea70d03267df0785f8c37fb5a
xRowe <33739292+xRowe@users.noreply.github.com>
2021-10-07 20:46:05 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -9,7 +9,7 @@
 git reset --hard cdb022d3e1cc0a57d16fdfa013382431118a9559
 mkdir package/kernel/i350
 #touch package/kernel/i350/Makefile
-cp ../I350_Makefile package/kernel/i350/
+cp ../i350_Makefile package/kernel/i350/Makefile
 
 # Uncomment a feed source
 sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
